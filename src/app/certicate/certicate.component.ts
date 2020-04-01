1e89974c3a088e6e90ca8a0e18682d55875d381d
ashish-fsd <ashish.bansal@oncehub.com>
2020-04-01 11:44:26 +0530

    certicate modified

--- src/app/certicate/certicate.component.ts
+++ src/app/certicate/certicate.component.ts
@@ -27,17 +27,17 @@ public convetToPDF()
 var data = document.getElementById('page-container');
 html2canvas(data).then(canvas => {
 // Few necessary setting options
-var imgWidth = 566;
-var pageHeight = 520;
+var imgWidth = 208;
+var pageHeight = 295;
 var imgHeight = canvas.height * imgWidth / canvas.width;
 var heightLeft = imgHeight;
+ 
 const contentDataURL = canvas.toDataURL('image/png')
 let pdf = new jspdf('l', 'mm', 'a4'); // A4 size page of PDF
 var position = 0;
-
-pdf.addImage(contentDataURL, 'PNG', 0, 0, imgWidth, imgHeight)
+pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight)
 pdf.save('new-file.pdf'); // Generated PDF
 });
 }
-;
+
 }
